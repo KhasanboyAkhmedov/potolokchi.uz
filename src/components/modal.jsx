51d7b70ddde5911f7362780825b27a46d4a68d1e
Khasanboy <paragon7400@gmail.com>
2024-09-03 21:44:08 +0500

    fix: text on onsend message

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -182,7 +182,7 @@ const Modal = ({open , handleOpen}) => {
                 {isLoading ? <div
                     className="inline-block h-8 w-8 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
                     role="status">
-                  </div> : (responseMessage ? (
+                  </div> : (responseMessage !== null && responseMessage.type ? (
                     `${responseMessage.text}`
                       ) : 'Отправить')}
             </button>
